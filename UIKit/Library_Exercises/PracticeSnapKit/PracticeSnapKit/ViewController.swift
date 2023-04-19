fd4ed7234c8d95739e5c872249eb3dfab1fd4c4a
kangddong <rkdehddud96@naver.com>
2023-04-19 15:25:01 +0900

    SnapKit 코드 업데이트

--- UIKit/Library_Exercises/PracticeSnapKit/PracticeSnapKit/ViewController.swift
+++ UIKit/Library_Exercises/PracticeSnapKit/PracticeSnapKit/ViewController.swift
@@ -4,6 +4,9 @@
 //
 //  Created by dongyeongkang on 2022/10/12.
 //
+// make -> update crashLayout -> Crash
+// make -> update Layout -> Pass
+// make -> remake Layout -> update crashLayout -> Pass
 
 import UIKit
 import SnapKit
@@ -12,13 +15,85 @@ class ViewController: UIViewController {
 
     lazy var box = UIView()
     
+    lazy var remakeButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.backgroundColor = .systemOrange
+        button.setTitle("remake Layout", for: .normal)
+        button.addTarget(self, action: #selector(remakeLayoutButton), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var crashButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.backgroundColor = .systemOrange
+        button.setTitle("update crashLayout", for: .normal)
+        button.addTarget(self, action: #selector(crashLayoutButton), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var updateButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.backgroundColor = .systemOrange
+        button.setTitle("update Layout", for: .normal)
+        button.addTarget(self, action: #selector(updateLayoutButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc
+    func remakeLayoutButton() {
+        box.snp.remakeConstraints { make -> Void in
+            make.top.equalTo(view).inset(100)
+            make.centerX.equalTo(view).inset(100)
+            make.size.equalTo(200)
+        }
+    }
+    
+    // make에 없는 제약 조건을 업데이트해서 Crash 나는 Case
+    // Fatal Error: Updated constraint could not find existing matching constraint to update:
+    @objc
+    func crashLayoutButton() {
+        box.snp.updateConstraints { make -> Void in
+            make.top.equalTo(view).inset(20)
+        }
+    }
+    
+    @objc
+    func updateLayoutButton() {
+        box.snp.updateConstraints { make -> Void in
+            make.size.equalTo(100)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.addSubview(box)
+        self.view.addSubview(remakeButton)
+        self.view.addSubview(crashButton)
+        self.view.addSubview(updateButton)
+        
         box.backgroundColor = .green
         box.snp.makeConstraints { make -> Void in
-            make.edges.equalTo(self.view)
+            make.center.equalTo(view)
+            make.size.equalTo(200)
+        }
+        
+        remakeButton.snp.makeConstraints { make -> Void in
+            make.leading.trailing.equalTo(updateButton)
+            make.bottom.equalTo(crashButton.snp.top).inset(-10)
+            make.height.equalTo(80)
+        }
+        
+        crashButton.snp.makeConstraints { make -> Void in
+            make.leading.trailing.equalTo(updateButton)
+            make.bottom.equalTo(updateButton.snp.top).inset(-10)
+            make.height.equalTo(80)
+        }
+        
+        updateButton.snp.makeConstraints { make -> Void in
+            make.leading.trailing.equalTo(view).inset(30)
+            make.bottom.equalTo(view).inset(50)
+            make.height.equalTo(80)
         }
     }
 
