b3808d400a92974efc476c7315b5badb071873f3
kangddong <rkdehddud96@naver.com>
2023-05-07 15:20:09 +0900

    Bridging-Header.h 및  NoticesViewController.hj, .m 생성

---
+++ UIKit/NSCFStringBridgeScratch/NSCFStringBridgeScratch/NoticesViewController.h
@@ -0,0 +1,16 @@
+//
+//  NoticesViewController.h
+//  NSCFStringBridgeScratch
+//
+//  Created by 강동영 on 2023/05/07.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NoticesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UIKit/NSCFStringBridgeScratch/NSCFStringBridgeScratch/NoticesViewController.m
@@ -0,0 +1,31 @@
+//
+//  NoticesViewController.m
+//  NSCFStringBridgeScratch
+//
+//  Created by 강동영 on 2023/05/07.
+//
+
+#import "NoticesViewController.h"
+
+@interface NoticesViewController ()
+
+@end
+
+@implementation NoticesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
