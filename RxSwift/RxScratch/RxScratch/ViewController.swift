774c35f2547e4273a2a21438725ef14bcd187a64
kangddong <rkdehddud96@naver.com>
2023-08-04 15:43:47 +0900

    just, of

--- RxSwift/RxScratch/RxScratch/ViewController.swift
+++ RxSwift/RxScratch/RxScratch/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let obserable1 = Observable.just(1)
+        obserable1.subscribe { event in // Event<Int>
+            print(event)
+        }
+        
+
+        let obserable2 = Observable.of(1, 2, 3)
+        obserable2.subscribe { event in // Event<Int>
+            print(event)
+        }
+        
+        let obserable3 = Observable.of([1, 2, 3])
+        obserable3.subscribe { event in // Event<Int>
+            print(event)
+        }
     }
-
-
 }
 
