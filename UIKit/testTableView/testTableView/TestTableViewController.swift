72c297b964a1b3afdbab3376c81c6ef09a064ab4
kangddong <rkdehddud96@naver.com>
2023-10-19 23:57:28 +0900

    동적 테이블뷰

--- UIKit/testTableView/testTableView/TestTableViewController.swift
+++ UIKit/testTableView/testTableView/TestTableViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 class TestTableViewController: UITableViewController {
 
     let worldClassList = ["BTS", "손흥민", "봉준호", "Rx"]
+    let testString = String(repeating: "안녕하세요", count: 10)
+    var worldClassList2 = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        for _ in 0...9 {
+            worldClassList2.append(testString)
+        }
+        tableView.register(TestTableViewCell.self, forCellReuseIdentifier: TestTableViewCell.reuseIdentifier)
+        tableView.separatorStyle = .none
+//        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.backgroundColor = .lightGray
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -30,12 +39,14 @@ class TestTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return worldClassList.count
+        return worldClassList2.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: TestTableViewCell.reuseIdentifier, for: indexPath)
+        guard let convertedCell = cell as? TestTableViewCell else { return cell }
+        convertedCell.setData(title: worldClassList2[indexPath.row])
         
         print("=============================================")
         print("indexPath: \(indexPath)")
@@ -43,10 +54,9 @@ class TestTableViewController: UITableViewController {
         print("indexPath.row: \(indexPath.row)")
         print("indexPath.item: \(indexPath.item)")
         print("=============================================")
-        
-        cell.textLabel?.text = worldClassList[indexPath.row]
+//        cell.textLabel?.text = worldClassList2[indexPath.row]
 
-        return cell
+        return convertedCell
     }
 
     /*
@@ -94,4 +104,48 @@ class TestTableViewController: UITableViewController {
     }
     */
 
+}
+
+class TestTableViewCell: UITableViewCell {
+    static let reuseIdentifier: String = "reuseIdentifier"
+    
+    let testLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.layer.borderWidth = 1
+        label.layer.borderColor = UIColor.red.cgColor
+        return label
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 16.0, left: 16, bottom: 16, right: 16))
+        
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        print("init")
+        self.contentView.addSubview(testLabel)
+        
+        let bottomAnchor = testLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -20)
+        bottomAnchor.priority = UILayoutPriority(250)
+        NSLayoutConstraint.activate([
+            testLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 20),
+            testLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 20),
+            testLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -20),
+            bottomAnchor,
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setData(title: String) {
+        testLabel.text = title
+    }
 }
