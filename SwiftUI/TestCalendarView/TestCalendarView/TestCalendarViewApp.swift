168efa2e155a39d4b7b5427d3bfdddb968c576f9
kangddong <rkdehddud96@naver.com>
2024-03-01 20:59:41 +0900

    [SwiftUI] Test Calendar View

---
+++ SwiftUI/TestCalendarView/TestCalendarView/TestCalendarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCalendarViewApp.swift
+//  TestCalendarView
+//
+//  Created by 강동영 on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestCalendarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
