6ef45560b8ea7f767d6b7dab2c174cddc29688ff
kangddong <rkdehddud96@naver.com>
2024-03-13 22:01:53 +0900

    [Done] stackview test

--- UIKit/Animation & Action/tapAlpha/tapAlpha/ViewController.swift
+++ UIKit/Animation & Action/tapAlpha/tapAlpha/ViewController.swift
@@ -7,67 +7,120 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    
+final class ViewController: UIViewController {
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var alphaHeaderView: UIView!
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var alphaTestButton: UIButton!
+    @IBOutlet weak var isHiddenTestButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var stackViewHeightLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(blinkHeaderView))
-        
-        let tap2: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(testTap))
-        
-        alphaHeaderView.addGestureRecognizer(tap2)
-        backgroundView.addGestureRecognizer(tap)
+        print("stackView height: \(stackView.frame.height)")
+        addButtonActions()
+        addTapgestures()
+        result()
     }
 
+    private func result() {
+        let text = alphaHeaderView.isHidden ? "No" : "Yes"
+        let resultText = "Is here test view ? -> \(text)"
+        
+        DispatchQueue.main.async {
+            self.resultLabel.text = resultText
+        }
+        
+        setStackViewHeight()
+    }
+    
+    private func setStackViewHeight() {
+        let resultText = "Pink Stackview's height ? : \(stackView.frame.height.description)"
+        
+        DispatchQueue.main.async {
+            self.stackViewHeightLabel.text = resultText
+        }
+    }
+    
+    private func addButtonActions() {
+        alphaTestButton.addTarget(self, action: #selector(testAlphaToHeaderView), for: .touchUpInside)
+        isHiddenTestButton.addTarget(self, action: #selector(testIsHiddenToHeaderView), for: .touchUpInside)
+    }
+    
+    private func addTapgestures() {
+        let alphaViewTapgesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(testisExistAlphaView))
+        
+        alphaHeaderView.addGestureRecognizer(alphaViewTapgesture)
+    }
+    
     @objc
-    func testTap() {
+    private func testisExistAlphaView() {
         print("exist View")
+        self.view.endEditing(true)
     }
         
     @objc
-    func blinkHeaderView() {
+    private func testAlphaToHeaderView() {
         print("blinkHeaderView")
         
-//        if alphaHeaderView.alpha == 0 {
-//            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseIn], animations: {
-//                self.alphaHeaderView.alpha = 0.5
-//                //self.alphaHeaderView.isHidden = false
-//            })
-//
-//        } else {
-//            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseIn], animations: {
-//                self.alphaHeaderView.alpha = 0.0
-//                //self.alphaHeaderView.isHidden = false
-//            })
-//        }
-        
-        if alphaHeaderView.isHidden {
-            //alphaHeaderView.isHidden = false
-            
-            UIView.animate(withDuration: 0.3,
-                           delay: 0,
-                           options: [.curveEaseIn],
-                           animations: {
-                self.alphaHeaderView.alpha = 0.5
-            })
-            
-        } else {
-            
-            UIView.animate(withDuration: 0.3,
-                           delay: 0,
-                           options: [.curveEaseIn],
-                           animations: {
+        if alphaHeaderView.alpha == 1.0 {
+            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseIn], animations: {
                 self.alphaHeaderView.alpha = 0.0
-            },
-                           completion: { _ in
-                //self.alphaHeaderView.isHidden.toggle()
             })
+            
+            DispatchQueue.main.async {
+                self.alphaTestButton.setTitle(
+                    "Test alpha 0.0 -> 1.0",
+                    for: .normal)
+            }
+        } else {
+            UIView.animate(withDuration: 0.2, delay: 0, options: [.curveEaseIn], animations: {
+                self.alphaHeaderView.alpha = 1.0
+            })
+            
+            DispatchQueue.main.async {
+                self.alphaTestButton.setTitle(
+                    "Test alpha 1.0 -> 0.0",
+                    for: .normal)
+            }
         }
     }
 
+    @objc
+    private func testIsHiddenToHeaderView() {
+        if alphaHeaderView.isHidden {
+            alphaHeaderView.isHidden = false
+            
+            UIView.animate(withDuration: 0.3,
+                           delay: 0,
+                           options: [],
+                           animations: {
+                self.stackView.layoutIfNeeded()
+            })
+            
+            DispatchQueue.main.async {
+                self.isHiddenTestButton.setTitle(
+                    "Test isHidden true -> false",
+                    for: .normal)
+                self.result()
+            }
+        } else {
+            alphaHeaderView.isHidden = true
+            UIView.animate(withDuration: 0.3,
+                           delay: 0,
+                           options: [],
+                           animations: {
+                self.stackView.layoutIfNeeded()
+            })
+            
+            DispatchQueue.main.async {
+                self.isHiddenTestButton.setTitle(
+                    "Test isHidden false -> true",
+                    for: .normal)
+                self.result()
+            }
+        }
+    }
 }
 
