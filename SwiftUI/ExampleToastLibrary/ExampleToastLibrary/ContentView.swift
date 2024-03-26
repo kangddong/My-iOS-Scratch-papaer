8ded65bb494537c75f31ccc50d6f48677440bd04
kangddong <rkdehddud96@naver.com>
2024-03-27 02:33:19 +0900

    project add

---
+++ SwiftUI/ExampleToastLibrary/ExampleToastLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleToastLibrary
+//
+//  Created by 강동영 on 3/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/ExampleToastLibrary/ExampleToastLibrary/ExampleToastLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleToastLibraryApp.swift
+//  ExampleToastLibrary
+//
+//  Created by 강동영 on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleToastLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
