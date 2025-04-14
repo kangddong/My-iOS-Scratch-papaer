e1c16d462e6365f607b6e32721a507a7044e8b66
kangddong <rkdehddud96@naver.com>
2025-04-14 22:20:58 +0900

    Network Failed 테스트

---
+++ ExNetworkFailed/ExNetworkFailed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExNetworkFailed
+//
+//  Created by 강동영 on 4/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
