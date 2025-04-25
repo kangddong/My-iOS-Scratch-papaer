c9f4d5bf7c545f5b0d6d8e9855e147a8414ca36e
kangddong <rkdehddud96@naver.com>
2025-04-25 17:15:10 +0900

    add: navi gesture test

---
+++ ExNaviSwipeGesture/ExNaviSwipeGesture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExNaviSwipeGesture
+//
+//  Created by 강동영 on 4/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExNaviSwipeGesture/ExNaviSwipeGesture/ExNaviSwipeGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExNaviSwipeGestureApp.swift
+//  ExNaviSwipeGesture
+//
+//  Created by 강동영 on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ExNaviSwipeGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
