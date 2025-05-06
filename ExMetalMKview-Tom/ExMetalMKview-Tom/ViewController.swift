d5f3054cdad391dd7e0b0a2c04d64afbcf76fec8
kangddong <rkdehddud96@naver.com>
2025-05-06 23:51:21 +0900

    add: 솔찬님 발표 Metal 코드

---
+++ ExMetalMKview-Tom/ExMetalMKview-Tom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExMetalMKview-Tom
+//
+//  Created by 강동영 on 5/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
