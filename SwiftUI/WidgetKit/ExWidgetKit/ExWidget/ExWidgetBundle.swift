2b6c3721ae5885d56369fc2acd6561bab98fff19
kangddong <rkdehddud96@naver.com>
2025-05-15 14:00:15 +0900

    add: WidgetKit

---
+++ SwiftUI/WidgetKit/ExWidgetKit/ExWidget/ExWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ExWidgetBundle.swift
+//  ExWidget
+//
+//  Created by 강동영 on 5/15/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ExWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ExWidget()
+    }
+}
