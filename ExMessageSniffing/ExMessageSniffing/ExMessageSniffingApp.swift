e873666bad8d11341297e4c6867fb9bf6df92237
kangddong <rkdehddud96@naver.com>
2025-05-16 19:58:44 +0900

    sms 필터링 앱

---
+++ ExMessageSniffing/ExMessageSniffing/ExMessageSniffingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExMessageSniffingApp.swift
+//  ExMessageSniffing
+//
+//  Created by 강동영 on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ExMessageSniffingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
