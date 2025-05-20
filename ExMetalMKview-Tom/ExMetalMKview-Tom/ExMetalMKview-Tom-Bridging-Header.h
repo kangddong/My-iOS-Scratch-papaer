a16472beeb132ba667f04d78a6b28ef406aaa9ca
kangddong <rkdehddud96@naver.com>
2025-05-20 18:57:10 +0900

    add: Metal Kit sample

---
+++ ExMetalMKview-Tom/ExMetalMKview-Tom/ExMetalMKview-Tom-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ExMetalMKview-Tom-Bridging-Header.h
+//  ExMetalMKview-Tom
+//
+//  Created by 강동영 on 5/20/25.
+//
+
+#ifndef ExMetalMKview_Tom_Bridging_Header_h
+#define ExMetalMKview_Tom_Bridging_Header_h
+
+
+#endif /* ExMetalMKview_Tom_Bridging_Header_h */
---
+++ ExMetalMKview-Tom/ExMetalMKview-Tom/TriangleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TriangleViewController.swift
+//  ExMetalMKview-Tom
+//
+//  Created by 강동영 on 5/20/25.
+//
+
+import UIKit
+
+class TriangleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
