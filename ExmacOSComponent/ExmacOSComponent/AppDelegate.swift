84087c42a389ac7cb210480ae24d3883a8bd11fe
kangddong <rkdehddud96@naver.com>
2025-05-22 17:29:59 +0900

    add: macOS 연습

---
+++ ExmacOSComponent/ExmacOSComponent/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ExmacOSComponent
+//
+//  Created by 강동영 on 5/22/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ ExmacOSComponent/ExmacOSComponent/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ExmacOSComponent
+//
+//  Created by 강동영 on 5/22/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
