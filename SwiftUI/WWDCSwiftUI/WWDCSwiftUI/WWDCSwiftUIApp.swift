79312dee520de142a23922e90ff8f88b2e3baad5
kangddong <rkdehddud96@naver.com>
2025-05-26 18:30:04 +0900

    WWDC SwiftUi 정리

---
+++ SwiftUI/WWDCSwiftUI/WWDCSwiftUI/WWDCSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCSwiftUIApp.swift
+//  WWDCSwiftUI
+//
+//  Created by 강동영 on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WWDCSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
