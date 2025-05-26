12660f2d5d14354928594f532447a95c15c4a11a
kangddong <rkdehddud96@naver.com>
2025-05-26 19:02:41 +0900

    add: TCA 연습

---
+++ ExTCA/ExTCA/ExTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExTCAApp.swift
+//  ExTCA
+//
+//  Created by 강동영 on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ExTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
