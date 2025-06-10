7242e350cbd33e37689b562fc0f0540021db65c0
kangddong <rkdehddud96@naver.com>
2025-06-10 17:52:33 +0900

    Xcode 26

---
+++ HelloXcode26/HelloXcode26/HelloXcode26App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcode26App.swift
+//  HelloXcode26
+//
+//  Created by 강동영 on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcode26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
